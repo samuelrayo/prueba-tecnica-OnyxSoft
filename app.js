0cce2ace3a701b9483bfa0cfc37c602ffa2e28d6
samuelrayo <apontersamuel@gmail.com>
2024-03-04 13:05:50 -0500

    Agrego configuración de express, gitignore y el json de los libros

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import { booksRouter } from './routes/booksRoutes.js'
+
+const app = express()
+app.use(express)
+
+app.disable('x-powered-by')
+
+app.use('/books', booksRouter)
+
+const PORT = process.env.PORT ?? 8080
+
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto http://localhost:${PORT}`)
+})
